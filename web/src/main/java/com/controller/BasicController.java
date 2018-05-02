57a9e4854e780b2c8bfc076e082eec8c1c27ff2a
zhou.xun <zhou.xun@qianka.com>
2018-05-02 18:22:59 +0800

    init layout

---
+++ web/src/main/java/com/controller/BasicController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by hey on 26/04/2018.
+ */
+@Controller
+public class BasicController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String printHello() throws Exception{
+        return "Get some fun";
+    }
+}
