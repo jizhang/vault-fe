88ee95e52859e2df426f8640a9a4f567474aad44
Ji ZHANG <zhangji87@gmail.com>
2018-06-10 11:10:24 +0800

    add element ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container style="height: 100vh;">
+      <el-header class="app-header">
+        <img src="./assets/logo.png">Morph
+      </el-header>
+      <el-container>
+        <el-aside width="240px">
+          <el-menu style="height: 100%;">
+            <el-menu-item>
+              <i class="el-icon-menu"></i>
+              <span>ETL</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -11,13 +27,28 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+}
+
+.app-header {
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 60px;
+  color: #303133;
+  box-shadow: 0 2px 8px #f0f1f2;
+  margin-bottom: 20px;
+
+  img {
+    height: 40px;
+    margin-right: 20px;
+    vertical-align: middle;
+  }
 }
 </style>
